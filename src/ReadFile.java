354da4f22e688d2fa4097a7db5dcf2702f7acd72
Sophia_Manodori <smanodori@smith.edu>
2023-12-04 15:10:41 -0500

    Reads in file

--- src/ReadFile.java
+++ src/ReadFile.java
@@ -1,10 +1,29 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class ReadFile {
-    public static void main(String[] args) {
-        String filename = "YUMA_2023.txt";
+
+    public static int fileLength (String filename) {
+        Scanner file = null;
+        try {
+          file = new Scanner(new File(filename));
+        } catch (FileNotFoundException e) {
+          System.err.println("Cannot locate file.");
+          System.exit(-1);
+        }
+        int i = 0;
+        while(file.hasNextLine()) {
+        i+=1;
+        file.nextLine();
+        }
+        file.close();
+        return i;
+      }
+
+    public static String[][] read(String[] args) {
+        String filename = "FILENAME";
         Scanner file = null;
         try {
             file = new Scanner(new File(filename));
@@ -12,8 +31,16 @@ public class ReadFile {
             System.err.println("Cannot locate file.");
             System.exit(-1);
         }
+        int n = fileLength(filename);
+        String[][] people = new String[n][];
 
-
+        for(int i = 0; i<n; i++) {
+            String line = file.nextLine();
+            String[] fields = line.split("\\s+");
+            people[i] = fields; 
+        }
+        return people;
 
     } 
+
 }

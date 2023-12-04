19b5a41276874727b087dc97e833b550ad2ac0ed
Sophia_Manodori <smanodori@smith.edu>
2023-12-04 10:42:22 -0500

    With new file

--- src/ReadFile.java
+++ src/ReadFile.java
@@ -1,3 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class ReadFile {
-    
+    public static void main(String[] args) {
+        String filename = "YUMA_2023.txt";
+        Scanner file = null;
+        try {
+            file = new Scanner(new File(filename));
+        } catch (FileNotFoundException e) {
+            System.err.println("Cannot locate file.");
+            System.exit(-1);
+        }
+
+
+
+    } 
 }

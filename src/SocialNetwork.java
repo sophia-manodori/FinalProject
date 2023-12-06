cc044fc6577018a8441deb744e1a1d535de6a776
Sophia_Manodori <smanodori@smith.edu>
2023-12-06 14:34:19 -0500

    Concise graph creator

--- src/SocialNetwork.java
+++ src/SocialNetwork.java
@@ -1,58 +1,53 @@
 import com.google.common.graph.GraphBuilder;
 import com.google.common.graph.MutableGraph;
+
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.HashMap;
+import java.util.Scanner;
 
 /** Social Network Class */
 public class SocialNetwork {
-    public MutableGraph<String[]> network; /** graph */
-    public HashMap<String, Integer> people = new HashMap<>(); /** hashmap with people and location in data */
-    public String[][] data;
+    public MutableGraph<String> network; /** graph */
+    public HashMap<String, String[]> people = new HashMap<>(); /** hashmap with people and location in data */
  
     /** Constructor */
-    public SocialNetwork() {
+    public SocialNetwork(String filename) {
+        Scanner file = null;
+        try {
+            file = new Scanner(new File(filename));
+        } catch (FileNotFoundException e) {
+            System.err.println("Cannot locate file.");
+            System.exit(-1);
+        }
+        //ArrayList<ArrayList<String>> people = new ArrayList<>();
+        this.people = new HashMap<>(); 
         this.network = GraphBuilder.undirected().build();
+
+        while(file.hasNextLine()) {
+            String line = file.nextLine().toLowerCase();
+            String[] fields = line.split("[,]+");
+            System.out.println(fields[0]);
+            people.put(fields[0], fields);
+            for(int i = 0; i<4; i++) {
+                //if()
+                network.putEdge(fields[0], fields[8+i]);
+            }
+        }
     }
     
-    /** */
-    public void CreateNetwork(String filename) {
-        int n = ReadFile.fileLength(filename);
-        this.data = ReadFile.read(filename, n);
-
-        for(int i = 0; i<n; i++) {
-            this.network.addNode(data[i]);
-            System.out.println(data[i][0]);
-            people.put(data[i][0], i);
-        }
-        for(int i = 0; i<n-8; i++) {
-            if(!(data[i][8].equals("NA")) && people.containsKey(data[i][8])) {
-                this.network.putEdge(data[i], data[people.get(data[i][8])]);
-            }
-            if(!(data[i].equals("NA")) && people.containsKey(data[i][9])) {
-                this.network.putEdge(data[i], data[people.get(data[i][9])]);
-            }
-            if(!(data[i][10].equals("NA")) && people.containsKey(data[i][10])) {
-                this.network.putEdge(data[i], data[people.get(data[i][10])]);
-            }
-            if(!(data[i][10].equals("NA")) && people.containsKey(data[i][11])) {
-                this.network.putEdge(data[i], data[people.get(data[i][11])]);
-            }
-        }
-    }
-
+    /** temporarily adds person for sake of interaction, won't add them to the data though so people won't be able to add them ...  */
     public void addPerson(String[] person) {
-        this.people.put(person[0], people.size());
-        this.network.addNode(person);
-        if(!(person[8].equals("NA")) && people.containsKey(person[8])) {
-                this.network.putEdge(person, data[people.get(person[8])]);
-        }
-        if(!(person[9].equals("NA")) && people.containsKey(person[9])) {
-                this.network.putEdge(person, data[people.get(person[9])]);
-        }
-        if(!(person[10].equals("NA")) && people.containsKey(person[10])) {
-                this.network.putEdge(person, data[people.get(person[9])]);
+        this.people.put(person[0], person);
+        this.network.addNode(person[0]);
+        for(int i = 0; i<4; i++) {
+            if(!(person[8].equals("na"))) {
+                    this.network.putEdge(person[0], person[i+8]);
+            }
         }
     }
 
+    //use nodes
     public void findFriends(String name, int degree) {
 
     }
@@ -66,8 +61,14 @@ public class SocialNetwork {
     }
 
     public static void main(String[] args) {
-        SocialNetwork test = new SocialNetwork();
-        test.CreateNetwork("Test.csv");
-        System.out.println(test.data[0][1]);
+        SocialNetwork test = new SocialNetwork("Test.csv");
+        for(String node : test.network.nodes()) {
+            System.out.println(node);
+        }
+
+        System.out.println(test.network);
+
+        GraphDisplay d3 = new GraphDisplay(test.network);
+    
     }
 }

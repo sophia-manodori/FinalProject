51a2b1a2ae832b911a82e4d71e32228ba34c30ac
Sophia_Manodori <smanodori@smith.edu>
2023-12-10 22:11:25 -0500

    main moved to main

--- src/main.java
+++ src/main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
-public class main {
+public class Main {
     public static boolean yesNoReader (String input) {
         if(input.toLowerCase().equals("yes") || input.toLowerCase().equals("y")) {
             return true;
@@ -12,35 +12,77 @@ public class main {
         }
     }
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        
-        SocialNetwork test = new SocialNetwork("Test.csv");
-        //maybe check here if we want to highlight a major?
-        boolean play = true; 
-        while(true) {
-            System.out.println("What would you like to do? type 'hobbie' to find mutual hobbie. Type 'number' to see the number of connections a person has"); //this will be where we list the options 
-            String response = input.nextLine();
-            //below is the option of finding all people with the same hobbie
-            if(response.equals("hobbie")) {
-                System.out.println("what hobbie?");
-                String hobbie = input.nextLine();
-                ArrayList<String> list = test.findHobbie(hobbie);
-                System.out.println("The people with the hobbie" + hobbie + " are: " + list.toString());
-            } else if(response.equals("number")) {
-                System.out.println("what person?");
-                String person = input.nextLine();
-                int numberfriends= test.numberFriends(person);
-                System.out.println("The number of friends of " + person + " are " + numberfriends);
-            }//other options will go here
-            else {
-                System.out.println("please enter a valid option");
+        System.out.println("");
+        System.out.println("Welcome to the Interactive Smith Social Network !");
+        System.out.println("This network allows you to find friends at Smith College, blah blah blah");
+            // I am not sure what to say hehehehe, should we like make it more fun or what
+        SocialNetwork socialNetwork = new SocialNetwork("Test.csv");
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("");
+            System.out.println("Options:");
+            System.out.println("1. Display all nodes in the network");
+            System.out.println("2. Find mutual friends between two people");
+            System.out.println("3. Find people with a specific hobby");
+            System.out.println("4. Find mutual hobbies for a person");
+            System.out.println("0. Exit");
+
+            System.out.print("Enter your choice: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Consume the newline character
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Nodes in the network:");
+                    for (String node : socialNetwork.network.nodes()) {
+                        System.out.println(node);
+                    }
+                    break;
+
+                case 2:
+                    System.out.print("Enter the first person's name: ");
+                    String name1 = scanner.nextLine();
+                    System.out.print("Enter the second person's name: ");
+                    String name2 = scanner.nextLine();
+                    System.out.print("Enter the degree: ");
+                    int degree = scanner.nextInt();
+                    socialNetwork.findMutualFriends(name1, name2, degree);
+
+                    break;
+
+                case 3:
+                    System.out.print("Enter the hobby to search for: ");
+                    String hobbie = scanner.nextLine();
+                    socialNetwork.findHobbie(hobbie);
+                   
+                    break;
+
+                case 4:
+                    System.out.print("Enter the person's name: ");
+                    String person = scanner.nextLine();
+                    System.out.print("Enter the degree: ");
+                    degree = scanner.nextInt();
+                    socialNetwork.findMutualHobbie(person, degree);
+
+                    break;
+
+                case 0:
+                    System.out.println("Exiting the program.");
+                    System.exit(0);
+
+                default:
+                    System.out.println("Invalid choice. Please try again.");
             }
-            //to quit the loop after an iteration of the interaction loop
-            System.out.println("would you like to continue?");
-            String quit = input.nextLine();
-            if(!yesNoReader(quit)) {
-                play = false;
+
+            // Ask the user if they want to perform more operations
+            System.out.print("Do you want to explore more? (yes/no): ");
+            String continueChoice = scanner.next().toLowerCase();
+            if (!continueChoice.equals("yes")) {
+                System.out.println("Exiting the program.");
+                System.exit(0);
             }
         }
+        }
     }
-}
+

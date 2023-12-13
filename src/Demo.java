a54638fa3cd189b05caa59dc09bf4f4d7ec48247
Sophia_Manodori <smanodori@smith.edu>
2023-12-13 10:28:36 -0500

    demo

---
+++ src/Demo.java
@@ -0,0 +1,8 @@
+public class Demo {
+    public static void main(String[] args) {
+            //this reads and creates the network 
+            SocialNetwork socialNetwork = new SocialNetwork("Test.csv");
+             
+            //below we display the network
+    }
+}
